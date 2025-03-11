bfa8e03d777cc2d5dd93ad47ef133b918d640e46
Cristian Ancutza <cristianancutza@gmail.com>
2025-03-10 21:37:54 -0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Definir la matriz de caracteres
+        var matrix = new List<string>
+        {
+            "coldw",
+            "hilla",
+            "illni",
+            "lndch",
+            "dwind"
+        };
+
+        // Definir las palabras a buscar
+        var wordStream = new List<string>
+        {
+            "chill", "cold", "wind", "hot", "rain"
+        };
+
+        // Instanciar la clase WordFinder
+        var wordFinder = new WordFinder(matrix);
+
+        // Buscar las palabras
+        var foundWords = wordFinder.Find(wordStream);
+
+        // Mostrar los resultados
+        Console.WriteLine("Palabras encontradas en la matriz:");
+        foreach (var word in foundWords)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
---
+++ ConsoleApp1/WordFinder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class WordFinder
+{
+    private readonly char[,] matrix;
+    private readonly int rows;
+    private readonly int cols;
+
+    public WordFinder(IEnumerable<string> matrix)
+    {
+        var matrixList = matrix.ToList();
+        rows = matrixList.Count;
+        cols = matrixList[0].Length;
+        this.matrix = new char[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                this.matrix[i, j] = matrixList[i][j];
+            }
+        }
+    }
+
+    public IEnumerable<string> Find(IEnumerable<string> wordstream)
+    {
+        var foundWords = new HashSet<string>();
+        var wordCount = new Dictionary<string, int>();
+
+        foreach (var word in wordstream.Distinct())
+        {
+            if (ExistsInMatrix(word))
+            {
+                foundWords.Add(word);
+                if (wordCount.ContainsKey(word))
+                    wordCount[word]++;
+                else
+                    wordCount[word] = 1;
+            }
+        }
+
+        return wordCount.OrderByDescending(x => x.Value)
+                        .Take(10)
+                        .Select(x => x.Key);
+    }
+
+    private bool ExistsInMatrix(string word)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            if (SearchHorizontal(i, word)) return true;
+        }
+
+        for (int j = 0; j < cols; j++)
+        {
+            if (SearchVertical(j, word)) return true;
+        }
+
+        return false;
+    }
+
+    private bool SearchHorizontal(int row, string word)
+    {
+        string rowStr = new string(Enumerable.Range(0, cols).Select(c => matrix[row, c]).ToArray());
+        return rowStr.Contains(word);
+    }
+
+    private bool SearchVertical(int col, string word)
+    {
+        string colStr = new string(Enumerable.Range(0, rows).Select(r => matrix[r, col]).ToArray());
+        return colStr.Contains(word);
+    }
+}
